75913fa117bfed28f4ae6b95ba92a59d07af2db7
Iovesophy <kazu825@me.com>
2021-10-17 12:05:33 +0900

    ADD test: directed graph phase

--- graph_test.go
+++ graph_test.go
@@ -1,6 +1,6 @@
 package graph
 
-func ExamplePrint() {
+func ExamplePrint_undirected() {
 	sampleNodeA := []Node{
 		{ID: 1, Element: "Node-A"},
 		{ID: 2, Element: "Node-B"},
@@ -17,6 +17,41 @@ func ExamplePrint() {
 	}
 	testGraph.CreateUndirectedVertexesPair()
 	testGraph.CreateUndirectedEdge()
+	testGraph.PrintGraph()
+	// Output:
+	// {{1 Node-A} {2 Node-B}}
+	// {{1 Node-A} {3 Node-C}}
+	// {{1 Node-A} {4 Node-D}}
+	// {{2 Node-B} {3 Node-C}}
+	// {{2 Node-B} {4 Node-D}}
+	// {{3 Node-C} {4 Node-D}}
+	// {{1 Node-A} {5 Node-E}}
+	// {{1 Node-A} {6 Node-F}}
+	// {{2 Node-B} {5 Node-E}}
+	// {{2 Node-B} {6 Node-F}}
+	// {{3 Node-C} {5 Node-E}}
+	// {{3 Node-C} {6 Node-F}}
+	// {{4 Node-D} {5 Node-E}}
+	// {{4 Node-D} {6 Node-F}}
+}
+
+func ExamplePrint_directed() {
+	sampleNodeA := []Node{
+		{ID: 1, Element: "Node-A"},
+		{ID: 2, Element: "Node-B"},
+		{ID: 3, Element: "Node-C"},
+		{ID: 4, Element: "Node-D"},
+	}
+	sampleNodeB := []Node{
+		{ID: 5, Element: "Node-E"},
+		{ID: 6, Element: "Node-F"},
+	}
+	testGraph := &NewGraph{
+		NodeA: sampleNodeA,
+		NodeB: sampleNodeB,
+	}
+	testGraph.CreateDirectedVertexesPair()
+	testGraph.CreateDirectedEdge()
 	testGraph.PrintGraph()
 	// Output:
 	// {{1 Node-A} {2 Node-B}}
